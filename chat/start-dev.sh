10195dbb59c526f13f065ce21d5edc27926afb7a
Gage <gage.tseng@geniecapital.com>
2011-08-03 15:42:35 +0800

    change path

--- chat/start-dev.sh
+++ chat/start-dev.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 cd `dirname $0`
 exec erl -pa $PWD/ebin \
-     -pa /Users/Gage/tmp/1337-CB-Chat/CB/ebin \
-     -pa /Users/Gage/tmp/1337-CB-Chat/CB/deps/*/ebin \
+     -pa ../CB/ebin \
+     -pa ../CB/deps/*/ebin \
      -boss developing_app chat \
      -boot start_sasl -config boss -s reloader -s boss \
      -sname wildbill
