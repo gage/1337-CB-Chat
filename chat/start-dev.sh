bb0768c816f91dcd68dc375c314c929d68c38f17
Gage <gage.tseng@geniecapital.com>
2011-08-03 16:25:31 +0800

    clean all

--- chat/start-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-exec erl -pa $PWD/ebin \
-     -pa ../CB/ebin \
-     -pa ../CB/deps/*/ebin \
-     -boss developing_app chat \
-     -boot start_sasl -config boss -s reloader -s boss \
-     -sname wildbill
