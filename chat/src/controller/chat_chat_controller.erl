43a04ffe4c3767b1da696b5f8cd6ae8cbec7f2ae
Gage <gage.tseng@geniecapital.com>
2011-08-15 22:39:38 +0800

    fixed all

--- chat/src/controller/chat_chat_controller.erl
+++ chat/src/controller/chat_chat_controller.erl
@@ -13,12 +13,32 @@ room('GET', [RoomName]) ->
     UserName = Req:query_param("name"),
     {ok, [{user_name, UserName}, {room_name, RoomName}, {timestamp, boss_mq:now(RoomName)}]}.
 
+get_time('GET', [RoomName]) ->
+    {json, [{timestamp, boss_mq:now(RoomName)}]}.
+
 pull('GET', [RoomName, Tt]) ->
     io:fwrite("4444:~s,~s\n", [RoomName, Tt]),
-    {ok, Timestamp, Messages} = boss_mq:pull(RoomName, list_to_integer(Tt)),
-    [Msg|_] = Messages,
-    io:fwrite("3333333:~s,~s,~s\n", [RoomName, Tt, Msg]),
-    {json, [{message,Msg}, {timestamp,Timestamp}]}.
+    case boss_mq:pull(RoomName, list_to_integer(Tt)) of
+        {ok, Timestamp, Messages} ->
+            io:fwrite("gggggggggg:~62p\n", [Messages]),
+            {json, [{messages,Messages}, {timestamp,Timestamp}]};
+        Other ->
+            io:fwrite("eeeeeeeee:~62p\n", [Other]),
+            {json, [{error, "error"}]}
+    end.
+    %{ok, Timestamp, Messages} = boss_mq:pull(RoomName, list_to_integer(Tt)),
+    %{ok, Timestamp, Messages} = boss_mq:pull(RoomName),
+    %io:fwrite("gggggggggg:~62p\n", [Messages]),
+    %{json, [{messages,Messages}, {timestamp,Timestamp}]}.
+
+    %case Messages of
+    %    [] -> {json, [{message,""}, {timestamp,Timestamp}]};
+    %    [Msg|_] -> {json, [{message,Msg}, {timestamp,Timestamp}]}
+    %end.
+
+    %[Msg|_] = Messages,
+    %io:fwrite("3333333:~s,~s,~s\n", [RoomName, Tt, Msg]),
+    %{json, [{message,Messages}, {timestamp,Timestamp}]}.
 
 message('POST', []) ->
     RoomName = Req:post_param("room_name"),
@@ -26,6 +46,9 @@ message('POST', []) ->
     Message = Req:post_param("msg"),
     io:fwrite("1111111:~s,~s,~s\n", [RoomName,UserName,Message]),
     io:fwrite("2222222:~s\n", [lists:concat([UserName,":",Message])]),
-    {ok, Timestamp} = boss_mq:push(RoomName, lists:concat([UserName,":",Message])),
+    {ok, Timestamp} = boss_mq:push(RoomName, [{username, UserName}, {msg, Message}]),
     {output, <<"success">>}.
 
+say('GET', []) ->
+    {ok, Timestamp, Messages} = boss_mq:pull("mtc", 'now'),
+    {output, Messages}.
